13d62e9a51957aa675b64d4c158602c12a1ec81e
NicoDevStuff <NicoG2008@gmx.de>
2022-10-09 20:59:23 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,285 @@
+-- ┏━┓╋┏┓╋╋╋╋╋╋┏━━━┓╋╋╋╋╋┏━━━┓┏┓╋╋╋╋┏━┓┏━┓
+-- ┃┃┗┓┃┃╋╋╋╋╋╋┗┓┏┓┃╋╋╋╋╋┃┏━┓┣┛┗┓╋╋╋┃┏┛┃┏┛
+-- ┃┏┓┗┛┣┳━━┳━━┓┃┃┃┣━━┳┓┏┫┗━━╋┓┏╋┓┏┳┛┗┳┛┗┓
+-- ┃┃┗┓┃┣┫┏━┫┏┓┃┃┃┃┃┃━┫┗┛┣━━┓┃┃┃┃┃┃┣┓┏┻┓┏┛
+-- ┃┃╋┃┃┃┃┗━┫┗┛┣┛┗┛┃┃━╋┓┏┫┗━┛┃┃┗┫┗┛┃┃┃╋┃┃
+-- ┗┛╋┗━┻┻━━┻━━┻━━━┻━━┛┗┛┗━━━┛┗━┻━━┛┗┛╋┗┛
+
+-- This is my neovim config rewritten in lua!! 
+-- [Links] -> https://gitlab.com/dwt1/dotfiles/-/blob/master/.config/nvim/init.lua 
+
+local g   = vim.g
+local o   = vim.o
+local opt = vim.opt
+local A   = vim.api
+
+-- base config
+o.number = true
+o.ruler = true
+o.mouse = "a"
+o.tabstop = 4
+o.shiftwidth = 4
+o.smarttab = true
+o.autoindent = true
+o.smartindent = true
+o.wrap = false
+o.encoding = "UTF-8"
+o.visualbell = true
+o.updatetime = 300
+opt.termguicolors = true
+o.showmode = false
+o.compatible = false
+o.clipboard = 'unnamedplus'
+o.splitright = true
+o.splitbelow = true
+
+-- set colorscheme
+vim.cmd([[colorscheme gruvbox]])
+
+g.mapleader = ' '
+--------------------
+
+-- keybindings
+
+local function map(m, k, v)
+   vim.keymap.set(m, k, v, { silent = true })
+end
+
+local builtin = require('telescope.builtin')
+
+map('n', '<leader>fb', builtin.find_files)
+map('n', '<leader>ff' , '<CMD>NvimTreeOpen<CR>')
+
+-- vim bufferline scroll through the tabs
+map('n', 'm', '<cmd> BufferLineCycleNext <CR>')
+map('n', 'n', '<cmd> BufferLineCyclePrev <CR>')
+map('n', '<leader>m', '<cmd> BufferLineMoveNext <CR>')
+map('n', '<leader>n', '<cmd> BufferLineMovePrev <CR>')
+
+-- terminal
+map('n', '<leader>tt', '<cmd> terminal <CR>')
+
+vim.cmd([[
+inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+							  ]])
+
+vim.cmd([[autocmd CursorHold * silent call CocActionAsync('highlight')]])
+
+vim.cmd([[inoremap <silent><expr> <c-space> coc#refresh()]])
+--------------------
+-- plugin configuration
+
+-- dashboard
+local db = require('dashboard')
+local home = os.getenv('HOME')
+
+function script_path()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*/)")
+end
+
+db.default_banner = {
+	'',
+	'',
+  	'  ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄   ▄▄ ',
+ 	' █  █  █ █       █       █  █ █  █   █  █▄█  █',
+ 	' █   █▄█ █    ▄▄▄█   ▄   █  █▄█  █   █       █',
+ 	' █       █   █▄▄▄█  █ █  █       █   █       █',
+ 	' █  ▄    █    ▄▄▄█  █▄█  █       █   █       █',
+ 	' █ █ █   █   █▄▄▄█       ██     ██   █ ██▄██ █',
+ 	' █▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄█▄█   █▄█',
+  	'',
+  	'',
+}
+
+db.preview_file_height = 11
+db.preview_file_width = 70
+db.custom_center = {
+	{icon = '  ',
+	desc = 'Recent sessions                         ',
+	shortcut = 'SPC s l',
+	action ='SessionLoad'},
+	{icon = '  ',
+	desc = 'Find recent files                       ',
+	action = 'Telescope oldfiles',
+	shortcut = 'SPC f r'},
+	{icon = '  ',
+	desc = 'Find files                              ',
+	action = 'Telescope find_files find_command=rg,--hidden,--files',
+	shortcut = 'SPC f f'},
+	{icon = '  ',
+	desc ='File browser                            ',
+	action =  'Telescope file_browser',
+	shortcut = 'SPC f b'},
+
+	{icon = '  ',
+	desc = 'Find word                               ',
+	action = 'Telescope live_grep',
+	shortcut = 'SPC f w'},
+	{icon = '  ',
+	desc = 'Load new theme                          ',
+	action = 'Telescope colorscheme',
+	shortcut = 'SPC h t'},
+}
+
+db.session_directory = "/home/nico/.config/nvim/session"
+db.custom_footer = { 'Path:', script_path() }
+
+-- coc.nvim
+g.coc_global_extensions = { 
+  'coc-snippets',
+  'coc-pairs',
+  'coc-eslint',
+  'coc-prettier',
+  'coc-json',
+} 
+
+-- -- NERDTree
+-- -- Exit Vim if NERDTree is the only window remaining in the only tab.
+-- vim.cmd([[
+-- 	autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+-- ]])
+
+-- -- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
+-- vim.cmd([[
+-- 	autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
+--     	\ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+-- ]])
+
+-- nvim-tree
+require("nvim-tree").setup {
+	auto_reload_on_write = true,
+	create_in_closed_folder = false,
+
+	diagnostics = {
+		enable = true,
+	},
+
+}
+
+
+-- notify
+vim.notify = require("notify")
+
+function notify(msg, title)
+	vim.notify(msg, nil, {
+		title = title,
+		render = "simple",
+		stages = "fade",
+	})
+end
+
+return require('packer').startup(function()
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- Dashboard is a nice start screen for nvim
+	use 'glepnir/dashboard-nvim'
+
+	-- Telescope and related plugins --
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use { "nvim-telescope/telescope-file-browser.nvim",
+		config = function()
+		require("telescope").setup {
+		  extensions = {
+			file_browser = {
+			  theme = "gruvbox",
+			  -- disables netrw and use telescope-file-browser in its place
+			  hijack_netrw = true,
+			},
+		  },
+		}
+		end
+	}
+
+	-- To get telescope-file-browser loaded and working with telescope,
+	-- you need to call load_extension, somewhere after setup function:
+	require("telescope").load_extension "file_browser"
+
+	-- Treesitter --
+	use {'nvim-treesitter/nvim-treesitter',
+	   config = function()
+		  require'nvim-treesitter.configs'.setup {
+		  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+		  -- highlighting will fallback to default Vim syntax highlighting
+			highlight = {
+			   enable = true,
+			   additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+			   },
+			ensure_installed = {'org'}, -- Or run :TSUpdate org
+			}
+	   end
+	}
+	
+
+	-- Productivity --
+	use 'vimwiki/vimwiki'
+	use 'jreybert/vimagit'
+	
+	 -- A better status line --
+	use { 'nvim-lualine/lualine.nvim',
+    	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  	}
+	require('lualine').setup()
+	-- File management --
+	use 'vifm/vifm.vim'
+	
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+    		'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  		},
+  		
+		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	}
+
+	use 'tiagofumo/vim-nerdtree-syntax-highlight'
+	use 'ryanoasis/vim-devicons'
+	
+	-- language server
+	use 'neoclide/coc.nvim'
+
+	-- Tim Pope Plugins --
+	use 'tpope/vim-surround'
+
+	-- Syntax Highlighting and Colors --
+	use 'PotatoesMaster/i3-vim-syntax'
+	use 'kovetskiy/sxhkd-vim'
+	use 'vim-python/python-syntax'
+	use 'ap/vim-css-color'
+
+	-- Junegunn Choi Plugins --
+	use 'junegunn/goyo.vim'
+	use 'junegunn/limelight.vim'
+	use 'junegunn/vim-emoji'
+
+	-- Colorschemes --
+	use 'RRethy/nvim-base16'
+	use 'kyazdani42/nvim-palenight.lua'
+	use 'morhetz/gruvbox'
+	use 'arcticicestudio/nord-vim'
+	use 'ghifarit53/tokyonight-vim'
+
+	-- Other stuff --
+	use 'frazrepo/vim-rainbow'
+	use 'rcarriga/nvim-notify'
+
+	use 'airblade/vim-gitgutter'
+	use 'itchyny/vim-gitbranch'
+
+	use 'tpope/vim-commentary'
+
+	use 'kyazdani42/nvim-web-devicons'
+	
+	use 'farmergreg/vim-lastplace'
+
+	-- bufferline --
+	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+	require("bufferline").setup()
+
+end)
---
+++ plugin/packer_compiled.lua
@@ -0,0 +1,279 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+_G._packer = _G._packer or {}
+_G._packer.inside_compile = true
+
+local time
+local profile_info
+local should_profile = false
+if should_profile then
+  local hrtime = vim.loop.hrtime
+  profile_info = {}
+  time = function(chunk, start)
+    if start then
+      profile_info[chunk] = hrtime()
+    else
+      profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+    end
+  end
+else
+  time = function(chunk, start) end
+end
+
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+  if threshold then
+    table.insert(results, '(Only showing plugins that took longer than ' .. threshold .. ' ms ' .. 'to load)')
+  end
+
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/home/nico/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/nico/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/nico/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/nico/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/nico/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  ["bufferline.nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
+    url = "https://github.com/akinsho/bufferline.nvim"
+  },
+  ["coc.nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    url = "https://github.com/neoclide/coc.nvim"
+  },
+  ["dashboard-nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/dashboard-nvim",
+    url = "https://github.com/glepnir/dashboard-nvim"
+  },
+  ["goyo.vim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/goyo.vim",
+    url = "https://github.com/junegunn/goyo.vim"
+  },
+  gruvbox = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/gruvbox",
+    url = "https://github.com/morhetz/gruvbox"
+  },
+  ["i3-vim-syntax"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/i3-vim-syntax",
+    url = "https://github.com/PotatoesMaster/i3-vim-syntax"
+  },
+  ["limelight.vim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/limelight.vim",
+    url = "https://github.com/junegunn/limelight.vim"
+  },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
+  ["nord-vim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nord-vim",
+    url = "https://github.com/arcticicestudio/nord-vim"
+  },
+  ["nvim-base16"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nvim-base16",
+    url = "https://github.com/RRethy/nvim-base16"
+  },
+  ["nvim-notify"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nvim-notify",
+    url = "https://github.com/rcarriga/nvim-notify"
+  },
+  ["nvim-palenight.lua"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nvim-palenight.lua",
+    url = "https://github.com/kyazdani42/nvim-palenight.lua"
+  },
+  ["nvim-tree.lua"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/nvim-tree/nvim-tree.lua"
+  },
+  ["nvim-treesitter"] = {
+    config = { "\27LJ\2\n¼\1\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\3\0005\4\4\0=\4\5\3=\3\a\0025\3\b\0=\3\t\2B\0\2\1K\0\1\0\21ensure_installed\1\2\0\0\borg\14highlight\1\0\0&additional_vim_regex_highlighting\1\2\0\0\borg\1\0\1\venable\2\nsetup\28nvim-treesitter.configs\frequire\0" },
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["python-syntax"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/python-syntax",
+    url = "https://github.com/vim-python/python-syntax"
+  },
+  ["sxhkd-vim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/sxhkd-vim",
+    url = "https://github.com/kovetskiy/sxhkd-vim"
+  },
+  ["telescope-file-browser.nvim"] = {
+    config = { "\27LJ\2\nˆ\1\0\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\4\0005\4\3\0=\4\5\3=\3\a\2B\0\2\1K\0\1\0\15extensions\1\0\0\17file_browser\1\0\0\1\0\2\17hijack_netrw\2\ntheme\fgruvbox\nsetup\14telescope\frequire\0" },
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
+    url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["tokyonight-vim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/tokyonight-vim",
+    url = "https://github.com/ghifarit53/tokyonight-vim"
+  },
+  ["vifm.vim"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vifm.vim",
+    url = "https://github.com/vifm/vifm.vim"
+  },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
+  },
+  ["vim-css-color"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-css-color",
+    url = "https://github.com/ap/vim-css-color"
+  },
+  ["vim-devicons"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-devicons",
+    url = "https://github.com/ryanoasis/vim-devicons"
+  },
+  ["vim-emoji"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-emoji",
+    url = "https://github.com/junegunn/vim-emoji"
+  },
+  ["vim-gitbranch"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-gitbranch",
+    url = "https://github.com/itchyny/vim-gitbranch"
+  },
+  ["vim-gitgutter"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    url = "https://github.com/airblade/vim-gitgutter"
+  },
+  ["vim-lastplace"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-lastplace",
+    url = "https://github.com/farmergreg/vim-lastplace"
+  },
+  ["vim-nerdtree-syntax-highlight"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-nerdtree-syntax-highlight",
+    url = "https://github.com/tiagofumo/vim-nerdtree-syntax-highlight"
+  },
+  ["vim-rainbow"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-rainbow",
+    url = "https://github.com/frazrepo/vim-rainbow"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
+  },
+  vimagit = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vimagit",
+    url = "https://github.com/jreybert/vimagit"
+  },
+  vimwiki = {
+    loaded = true,
+    path = "/home/nico/.local/share/nvim/site/pack/packer/start/vimwiki",
+    url = "https://github.com/vimwiki/vimwiki"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+-- Config for: nvim-treesitter
+time([[Config for nvim-treesitter]], true)
+try_loadstring("\27LJ\2\n¼\1\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\3\0005\4\4\0=\4\5\3=\3\a\0025\3\b\0=\3\t\2B\0\2\1K\0\1\0\21ensure_installed\1\2\0\0\borg\14highlight\1\0\0&additional_vim_regex_highlighting\1\2\0\0\borg\1\0\1\venable\2\nsetup\28nvim-treesitter.configs\frequire\0", "config", "nvim-treesitter")
+time([[Config for nvim-treesitter]], false)
+-- Config for: telescope-file-browser.nvim
+time([[Config for telescope-file-browser.nvim]], true)
+try_loadstring("\27LJ\2\nˆ\1\0\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\4\0005\4\3\0=\4\5\3=\3\a\2B\0\2\1K\0\1\0\15extensions\1\0\0\17file_browser\1\0\0\1\0\2\17hijack_netrw\2\ntheme\fgruvbox\nsetup\14telescope\frequire\0", "config", "telescope-file-browser.nvim")
+time([[Config for telescope-file-browser.nvim]], false)
+
+_G._packer.inside_compile = false
+if _G._packer.needs_bufread == true then
+  vim.cmd("doautocmd BufRead")
+end
+_G._packer.needs_bufread = false
+
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
