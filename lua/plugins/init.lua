6c4809226d23236d8bbab275c7b22e39bd4438e5
NicoDevStuff <NicoG2008@gmx.de>
2022-10-11 14:42:57 +0200

    Cleaned up plugin init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,15 +2,11 @@ return require('packer').startup(function()
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
-	-- Dashboard is a nice start screen for nvim
 	use 'glepnir/dashboard-nvim'
-
-	-- Telescope and related plugins --
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-
 	use { "nvim-telescope/telescope-file-browser.nvim",
 		config = function()
 		require("telescope").setup {
@@ -24,12 +20,8 @@ return require('packer').startup(function()
 		}
 		end
 	}
-
-	-- To get telescope-file-browser loaded and working with telescope,
-	-- you need to call load_extension, somewhere after setup function:
 	require("telescope").load_extension "file_browser"
 
-	-- Treesitter --
 	use {'nvim-treesitter/nvim-treesitter',
 	   config = function()
 		  require'nvim-treesitter.configs'.setup {
@@ -43,21 +35,15 @@ return require('packer').startup(function()
 			}
 	   end
 	}
-	
 
-	-- Productivity --
 	use 'vimwiki/vimwiki'
 	use 'jreybert/vimagit'
-	
-	 -- A better status line --
 	use { 'nvim-lualine/lualine.nvim',
     	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   	}
 	require('lualine').setup()
 	
-	-- File management --
 	use 'vifm/vifm.vim'
-	
 	use {
 		'nvim-tree/nvim-tree.lua',
 		requires = {
@@ -66,51 +52,32 @@ return require('packer').startup(function()
   		
 		tag = 'nightly' -- optional, updated every week. (see issue #1193)
 	}
-
 	use 'tiagofumo/vim-nerdtree-syntax-highlight'
 	use 'ryanoasis/vim-devicons'
-	
-	-- language server
 	use 'neoclide/coc.nvim'
 	use 'sheerun/vim-polyglot'
 	use 'rust-lang/rust.vim'
-	-- Tim Pope Plugins --
 	use 'tpope/vim-surround'
-
-	-- Syntax Highlighting and Colors --
 	use 'PotatoesMaster/i3-vim-syntax'
 	use 'kovetskiy/sxhkd-vim'
 	use 'vim-python/python-syntax'
 	use 'ap/vim-css-color'
-
-	-- Junegunn Choi Plugins --
 	use 'junegunn/goyo.vim'
 	use 'junegunn/limelight.vim'
 	use 'junegunn/vim-emoji'
-
-	-- Colorschemes --
 	use 'RRethy/nvim-base16'
 	use 'kyazdani42/nvim-palenight.lua'
 	use 'morhetz/gruvbox'
 	use 'arcticicestudio/nord-vim'
 	use 'ghifarit53/tokyonight-vim'
-
-	-- Other stuff --
 	use 'frazrepo/vim-rainbow'
 	use 'rcarriga/nvim-notify'
-
 	use 'airblade/vim-gitgutter'
 	use 'itchyny/vim-gitbranch'
-
 	use 'tpope/vim-commentary'
-
 	use 'kyazdani42/nvim-web-devicons'
-	
 	use 'farmergreg/vim-lastplace'
-
 	use 'dstein64/vim-startuptime'
-
-	-- bufferline --
 	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
 	require("bufferline").setup()
 
@@ -127,5 +94,3 @@ return require('packer').startup(function()
 	}
 
 end)
-
-
